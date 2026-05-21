55949fe963b3934204b6e1485daa415ddca5c5a6
JaProy <alexandersotelo423@gmail.com>
2026-05-21 13:54:53 -0600

    I changed the security.

--- src/main/java/com/alexander/springboot/r_siiau_backend/security/SecurityConfiguration.java
+++ src/main/java/com/alexander/springboot/r_siiau_backend/security/SecurityConfiguration.java
@@ -78,7 +78,7 @@ public class SecurityConfiguration {
                 "http://localhost:[*]",
                 "http://127.0.0.1:[*]",
                 "http://192.168.*.*:[*]",
-                "https://compstock-c3qo.onrender.com"
+                "https://r-siiau.onrender.com"
         ));
 
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
